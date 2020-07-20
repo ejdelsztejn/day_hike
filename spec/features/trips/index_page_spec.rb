8c427e756eb0ff4ba10cacf6caa1a1fead3f9f88
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-07-20 09:12:25 -0400

    Add can link to a unique page for each hiking trip spec

--- spec/features/trips/index_page_spec.rb
+++ spec/features/trips/index_page_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'hiking trips index page', type: :feature do
+  before(:each) do
+    @peaked_mountain = Trip.create(name: "Peaked Mountain", start_date: '2020-07-17', end_date: '2020-07-17')
+    @tantiusques = Trip.create(name: "Tantiusques", start_date: '2020-07-18', end_date: '2020-07-18')
+    @peaked_mountain_1 = @peaked_mountain.trails.create(length: 1, name: "Lunden Pond Loop", address: "43 Butler Road")
+    @peaked_mountain_2 = @peaked_mountain.trails.create(length: 2, name: "Miller Forest Trail", address: "43 Butler Road")
+    @tantiusques_1 = @tantiusques.trails.create(length: 2, name: "Tantiusques Loop", address: "493 Leadmine Rd")
+    TripTrail.create(trip: @peaked_mountain, trail: @peaked_mountain_1)
+    TripTrail.create(trip: @peaked_mountain, trail: @peaked_mountain_2)
+    TripTrail.create(trip: @tantiusques, trail: @tantiusques_1)
+  end
   it 'can display a list of all hiking trip names' do
     peaked_mountain = Trip.create(name: "Peaked Mountain", start_date: '2020-07-17', end_date: '2020-07-17')
     tantiusques = Trip.create(name: "Tantiusques", start_date: '2020-07-18', end_date: '2020-07-18')
@@ -13,7 +23,16 @@ RSpec.describe 'hiking trips index page', type: :feature do
 
     visit '/trips'
 
-    expect(page).to have_content("Peaked Mountain")
-    expect(page).to have_content("Tantiusques")
+    expect(page).to have_content("#{@peaked_mountain.name}")
+    expect(page).to have_content("#{@tantiusques.name}")
+  end
+  it 'can link to a unique page for each hiking trip' do
+    visit '/trips'
+
+    expect(page).to have_link("Peaked Mountain")
+
+    click_on "Peaked Mountain"
+
+    expect(current_path).to eq("/trips/#{@peaked_mountain.id}")
   end
 end
