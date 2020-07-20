dbe14a44ba6cb1ad410488db09e8a39aa8d344c7
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-07-20 09:04:16 -0400

    Fix validations for Trip, add trip index page spec

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,5 @@
 class Trip < ApplicationRecord
-  validates_presence_of :name
+  validates_presence_of :name, :start_date, :end_date
   has_many :trip_trails
   has_many :trails, through: :trip_trails
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe Trip, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
+    it { should validate_presence_of :start_date }
+    it { should validate_presence_of :end_date }
 
     it { should have_many(:trails).through(:trip_trails) }
   end
